5263ddb3be5034e008b37b774f9c45b220ce8b98
AyoubM <ubmagh@gmail.com>
2021-05-27 17:13:40 +0100

    Step 15 : creation the file 'respositories/users.js' to manage users

---
+++ respositories/users.js
@@ -0,0 +1,22 @@
+const { User } = require('../models')
+
+module.exports = {
+    getAllUsers() {
+        return User.findAll()
+    },
+    
+    // méthodes à implémenter
+    getUsers(offset = 0, limit = 10) { },
+    getAdmins() { },
+    getAuthors() { },
+    getGuests(){ },
+    getUser(id) { },
+    getUserByEmail(email) { },
+    addUser(user) { },
+    updateUser() { },
+    deleteUser() { },
+    
+    // D'autres méthodes jugées utiles
+
+}
+    
