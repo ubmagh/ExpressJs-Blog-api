7d48a2ce47154d4ab88b179d5212f004f446610e
AyoubM <ubmagh@gmail.com>
2021-06-03 23:07:52 +0100

    Step 22 : No code Changes, just the Routes table has been drawed in the report

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use( logger('dev') );
+app.use( express.json() );
+app.use( express.urlencoded({ extended: false }) );
+app.use( cookieParser());
+app.use( express.static( path.join( __dirname, 'public')) );
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use( '/', indexRouter);
+app.use( '/users', usersRouter);
 
 module.exports = app;
